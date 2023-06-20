231bea24a4bea88accd996a38ebef4c5231b350a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-20 14:10:11 -0500

    initial commit

--- SwiftUIGesturesDemo/ContentView.swift
+++ SwiftUIGesturesDemo/ContentView.swift
@@ -1,19 +1,42 @@
-//
-//  ContentView.swift
-//  SwiftUIGesturesDemo
-//
-//  Created by Mark Volkmann on 6/20/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState private var deltaRotation = Angle.zero
+    @GestureState private var deltaScale = 1.0
+
+    @State private var currentScale = 1.0
+    @State private var currentRotation = Angle.zero
+
+    private var magnification: some Gesture {
+        return MagnificationGesture()
+            .updating($deltaScale) { value, state, _ in
+                state = value
+            }
+            .onEnded { currentScale *= $0 }
+    }
+
+    private var rotation: some Gesture {
+        return RotationGesture()
+            .updating($deltaRotation) { value, state, _ in
+                state = value
+            }
+            .onEnded { currentRotation += $0 }
+    }
+
     var body: some View {
         VStack {
+            Text("currentScale = \(currentScale)")
+            Text("deltaScale = \(deltaScale)")
+            Text("currentRotation = \(currentRotation.degrees)")
+            Text("deltaRotation = \(deltaRotation.degrees)")
             Image(systemName: "globe")
-                .imageScale(.large)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .rotationEffect(currentRotation + deltaRotation)
+                .scaleEffect(currentScale * deltaScale)
+                // Note use of `.simultaneously` to support multiple gestures.
+                .gesture(magnification.simultaneously(with: rotation))
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
     }
--- SwiftUIGesturesDemo/SwiftUIGesturesDemoApp.swift
+++ SwiftUIGesturesDemo/SwiftUIGesturesDemoApp.swift
@@ -1,10 +1,3 @@
-//
-//  SwiftUIGesturesDemoApp.swift
-//  SwiftUIGesturesDemo
-//
-//  Created by Mark Volkmann on 6/20/23.
-//
-
 import SwiftUI
 
 @main
