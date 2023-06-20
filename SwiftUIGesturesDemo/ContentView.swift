9608c36cb70dc602e29251d96bcec2e873648d3c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-20 14:09:17 -0500

    Initial Commit

---
+++ SwiftUIGesturesDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIGesturesDemo
+//
+//  Created by Mark Volkmann on 6/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGesturesDemo/SwiftUIGesturesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGesturesDemoApp.swift
+//  SwiftUIGesturesDemo
+//
+//  Created by Mark Volkmann on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGesturesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
